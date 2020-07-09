8a90195826e1fec91f80c0dd55c396b33b793767
Frederik Glücks <frederik@gluecks-gmbh.de>
2020-07-09 23:04:31 +0200

    Add JSON template Engine for JSON output

---
+++ libs/MVC/TemplateEngines/JSON/ParentController.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * ParentController
+ *
+ * @package    BASEs
+ * @subpackage MVC/JSON
+ * @author         Frederik Glücks <frederik@gluecks-gmbh.de>
+ * @license        lgpl-3.0
+ *
+ */
+
+namespace BASE\MVC\TemplateEngines\JSON;
+
+use Exception;
+
+/**
+ * Class ParentController
+ *
+ * @package    BASE
+ * @subpackage MVC
+ * @version    v0.
+ * @author         Frederik Glücks <frederik@gluecks-gmbh.de>
+ * @license        lgpl-3.0
+ *
+ */
+class ParentController implements \BASE\MVC\TemplateEngines\ParentController
+{
+    /**
+     * @var string $code language-country-code
+     */
+    protected string $code = "";
+
+    /**
+     * ParentController constructor.
+     *
+     * @param string $code language-country-code
+     */
+    public function __construct(string $code)
+    {
+        $this->code = $code;
+    }
+
+    /**
+     *
+     * @param array $data
+     *
+     * @throws Exception
+     */
+    protected function createJsonOutput(array $data): void
+    {
+        header('Access-Control-Allow-Origin: *');
+        header("Content-type: application/json; charset=utf-8");
+        echo json_encode($data);
+    }
+}
